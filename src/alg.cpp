f947dea9abd3b377ca76165430ee6bd03d991fce
Sakhno Elizaveta <97175260+elizaveta08@users.noreply.github.com>
2022-04-02 21:35:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,33 +3,34 @@
 #include <map>
 #include "tstack.h"
 std::string infx2pstfx(std::string inf) {
-  // Функция infx2pstfx должна преобразовывать входную строку, содержащую выражение в инфиксной форме в выходную строку,
-  // которая содержит то же выражение в постфиксной фоме. 
+  // Функция infx2pstfx должна преобразовывать входную строку,
+  // содержащую выражение в инфиксной форме в выходную строку,
+  // которая содержит то же выражение в постфиксной фоме.
   // Для отделения операндов друг от друга нужно использовать пробелы.
   std::string resultat;
   char probel = ' ';
   TStack <char, 100> stack;
   for (int i = 0; i < inf.size(); i++) {
-    if (priority(inf[i]) == 4) {
+    if (prior(inf[i]) == 4) {
       resultat.push_back(inf[i]);
       resultat.push_back(probel);
     } else {
-      if (priority(inf[i]) == 0) {
+      if (prior(inf[i]) == 0) {
         stack.push(inf[i]);
       } else if (stack.isEmpty()) {
           stack.push(inf[i]);
-      } else if ((priority(inf[i]) > priority(stack.get()))) {
+      } else if ((priority(inf[i]) > prioy(stack.get()))) {
           stack.push(inf[i]);
-      } else if (priority(inf[i]) == 1) {
-        while (priority(stack.get()) != 0) {
+      } else if (prior(inf[i]) == 1) {
+        while (prior(stack.get()) != 0) {
           resultat.push_back(stack.get());
           resultat.push_back(probel);
           stack.pop();
         }
         stack.pop();
       } else {
-        char a = priority(inf[i]);
-        char b = priority(stack.get());
+        char a = prior(inf[i]);
+        char b = prior(stack.get());
         while ((a <= b) && (!stack.isEmpty())) {
           resultat.push_back(stack.get());
           resultat.push_back(probel);
@@ -44,7 +45,7 @@ std::string infx2pstfx(std::string inf) {
     resultat.push_back(probel);
     stack.pop();
   }
-  for (int i = 0; i < res.size(); i++) {
+  for (int i = 0; i < resultat.size(); i++) {
     if (resultat[resultat.size() - 1] == ' ')
       resultat.erase(resultat.size() - 1);
   }
@@ -80,7 +81,9 @@ std::string infx2pstfx(std::string inf) {
   return std::string("");
 }
 int eval(std::string pref) {
-  // Функция eval должна вычислять выражение, записанное в постфиксном виде и выдавать целое значение. Все расчеты ведутся в целых числах!
+  // Функция eval должна вычислять выражение,
+  // записанное в постфиксном виде и выдавать целое значение.
+  // Все расчеты ведутся в целых числах!
   TStack <int, 100> stack2;
   int zeloeznachenie = 0;
   for (int i = 0; i < pref.size(); i++) {
